bf6f6bd2b14190caf48641524fbd88fa225853cc
Rafal Dubiel <rafal.dubiel@elitecrew.io>
2020-06-14 23:55:16 +0200

    JOP-8 Added methods for get/create/update/delete job tag.

--- JobOffersPortal/restApi/views.py
+++ JobOffersPortal/restApi/views.py
@@ -4,7 +4,7 @@ from rest_auth.registration.views import RegisterView
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
-from .serializers import JobOfferSerializer, TagsSerializer
+from .serializers import JobOfferSerializer, JobTagsSerializer
 from .models import User, JobOffer, JobTag
 from .permissions import IsEmployer, IsOwner
 
@@ -52,9 +52,57 @@ class JobOffersViewSet(viewsets.ViewSet):
     def delete(self, request, pk = None):
         try:
             JobOffer.objects.get(pk = pk).delete()
-            return Response(request.data, status = status.HTTP)
+            return Response(request.data, status = status.HTTP_200_OK)
         except JobOffer.DoesNotExist:
             content = {
-                'status' : 'Resource does not exist'
+                'status': 'Resource does not exist'
+            }
+            return Response(content, status = status.HTTP_404_NOT_FOUND)
+
+class JobTagsViewSet(viewsets.ViewSet):
+    serializer_class = JobTagsSerializer
+    permission_classes = (IsEmployer, IsOwner)
+
+    def list(self, request):
+        tags = JobTag.objects.all()
+        serializer = JobTagsSerializer(tags, many = True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = JobTagsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk = None):
+        queryset = JobTag.objects.all()
+        tag = get_object_or_404(queryset, pk = pk)
+        serializer = JobTagsSerializer(tag)
+        return Response(serializer.data)
+
+    def update(self, request, pk = None):
+        try:
+            instance = JobTag.objects.get(pk = pk)
+            self.check_object_permissions(request, instance)
+            serializer = JobTagsSerializer(instance = instance, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = status.HTTP_200_OK)
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        except JobTag.DoesNotExist:
+            serializer = JobTagsSerializer(data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status = status.HTTP_201_CREATED)
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk = None):
+        try:
+            JobTag.objects.get(pk = pk).delete()
+            return Response(request.data, status = status.HTTP_200_OK)
+        except JobTag.DoesNotExist:
+            content = {
+                'status': 'Resource does not exist'
             }
             return Response(content, status = status.HTTP_404_NOT_FOUND)
