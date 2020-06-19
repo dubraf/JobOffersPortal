ae53956f1ca819a9e04c77270b2251ad9dbe2b80
Rafal Dubiel <rafal.dubiel@elitecrew.io>
2020-06-19 21:46:39 +0200

    JOP-12 Removed not need files.

--- JobOffersPortal/settings.py
+++ JobOffersPortal/settings.py
@@ -46,13 +46,9 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'rest_auth',
 ]
-ALLOWED_HOSTS = ['.herokuapp.com']
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+ALLOWED_HOSTS = ['*']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
